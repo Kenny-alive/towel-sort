a7fbba716bc5407c7ba54a42b1c7872488b1f27d
Kenny-alive <ksvcsv@inbox.ru>
2024-08-10 14:57:13 +0300

    beach boy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
+// Your task is to implement the so-called 'towel sort' algorithm.
 
-// You should implement your task here.
+// `towelSort` function should expect [matrix](https://en.wikipedia.org/wiki/Array_data_structure#Multidimensional_arrays) of any shape for example:
 
-module.exports = function towelSort (matrix) {
-  return [];
+// ```js
+// [
+//  [ 1, 2, 3 ],
+//  [ 4, 5, 6 ],
+//  [ 7, 8, 9 ],
+// ]
+// ```
+
+// Following matrix should be 'sorted' to:
+// ```
+//  [1, 2, 3, 6, 5, 4, 7, 8, 9 ]
+// ```
+
+
+
+
+module.exports = function towelSort(matrix) {
+
+  if (matrix === undefined || matrix.length == 0) {
+    return [];
+  }
+  matrix = matrix.map((item, index) => {
+    if (index % 2 == 0) { return item }
+    return item.reverse()
+  });
+  console.log("🚀 ~ towelSort ~ matrix:", matrix)
+  return matrix = JSON.stringify(matrix).replace(/(\[|\])/g, '').split(',').map(item => Number(item));
 }
